c8c77503e851cc8813e0c944cf90050becdb5e6b
Yeakub1 <mryeakub10@gmail.com>
2024-01-16 14:53:25 +0600

    design the counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from "./redux/features/CounterSlice";
 
 const App = () => {
+  const {count} = useSelector((state)=> state.counter)
+  const dispatch = useDispatch()
   return (
     <div className="h-screen w-full flex justify-center items-center">
-    <div className="flex border-purple-700 rounded-md bg-slate-200 p-10">
-      <button className="px-4 py-2 bg-green-600 text-xl font-bold text-white rounded-md">
-        Increment
-      </button>
-      <h1 className="text-3xl mx-10">0</h1>
-      <button className="px-4 py-2 bg-red-600 text-xl font-bold text-white rounded-md">
-        Decrement
-      </button>
-    </div>
+      <div className="flex border-purple-700 rounded-md bg-slate-200 p-10">
+        <button
+          onClick={() => dispatch(increment())}
+          className="px-4 py-2 bg-green-600 text-xl font-bold text-white rounded-md"
+        >
+          Increment
+        </button>
+        <h1 className="text-3xl mx-10">{ count}</h1>
+        <button
+          onClick={() => dispatch(decrement())}
+          className="px-4 py-2 bg-red-600 text-xl font-bold text-white rounded-md"
+        >
+          Decrement
+        </button>
+      </div>
     </div>
   );
 };
--- src/redux/features/CounterSlice.ts
+++ src/redux/features/CounterSlice.ts
@@ -3,7 +3,17 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = { count: 0 };
 
 const counterSlice = createSlice({
-    name: 'counter',
-    initialState,
-    reducers:{}
-})
+  name: "counter",
+  initialState,
+  reducers: {
+    increment: (starte, action) => {
+      starte.count = starte.count + 1;
+    },
+    decrement: (state, action) => {
+      state.count = state.count - 1;
+    },
+  },
+});
+
+export const { increment, decrement } = counterSlice.actions;
+export default counterSlice.reducer; 
