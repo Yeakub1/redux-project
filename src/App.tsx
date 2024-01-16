9cc23ed5a26bd9c8babca248ef88121cbf24646c
Yeakub1 <mryeakub10@gmail.com>
2024-01-16 13:13:30 +0600

    initialSlice

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 
 const App = () => {
   return (
-    <div>
-      <h1>React with reducx</h1>
+    <div className="h-screen w-full flex justify-center items-center">
+    <div className="flex border-purple-700 rounded-md bg-slate-200 p-10">
+      <button className="px-4 py-2 bg-green-600 text-xl font-bold text-white rounded-md">
+        Increment
+      </button>
+      <h1 className="text-3xl mx-10">0</h1>
+      <button className="px-4 py-2 bg-red-600 text-xl font-bold text-white rounded-md">
+        Decrement
+      </button>
+    </div>
     </div>
   );
 };
